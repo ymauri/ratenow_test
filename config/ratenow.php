ec63edf73b0b2acc8f0f397b5f0ee3ebecbe4d74
Yolanda Mauri Perez <ymauri@liddeo.com>
2021-07-29 00:01:50 +0200

    Config field for rate now params

---
+++ config/ratenow.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'url' => env('RATENOW_API_URL', 'http://test.ratenow.es:8080/'),
+
+    'client_id' => env('RATENOW_API_CLIENT_ID', 'TecnicTest'),
+
+    'client_secret' => env('RATENOW_API_CLIENT_SECRET', 'TecnicTest'),
+
+    'user' => env('RATENOW_API_USER', 'YMauri'),
+
+    'password' => env('RATENOW_API_PASSWORD', 'RN-YMauri')
+
+];
